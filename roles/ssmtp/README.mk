d47d21e9f2425f08a7c09976157564abd56cf838
Florestan Bredow <florestan.bredow@supagro.fr>
2017-05-26 10:48:44 +0200

    Ajoute role ssmtp

---
+++ roles/ssmtp/README.mk
@@ -0,0 +1,11 @@
+Role ssmtp pour ansible
+=======================
+
+Utilise un SMTP extérieur pour envoyer les mails des serveurs.
+
+Variables :
+ - ssmtp_username
+ - ssmtp_password
+ - ssmtp_mailhub
+ - ssmtp_host
+ - ssmtp_port
